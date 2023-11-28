54b9ff3a339cb72fc6e394c4fafdbe59c11e1f5e
hotwater <vlad.dalechin@gmail.com>
2023-11-28 14:23:05 +0200

    feat: completions and tmux
    I actually forgot to include tmux vim config.
    This commit also changes the behaviour of `nvim-cmp` to
    be triggered on <Tab> and <S-Tab> to scroll through completions.

---
+++ .config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,45 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local has_words_before = function()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    -- local diagnostics_options = require("config.defaults").diagnostics_options
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete({ TriggerOnly = "triggerOnly" }),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-e>"] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+  end,
+}
