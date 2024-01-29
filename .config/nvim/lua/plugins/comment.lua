7a684958b1c4f6aaa79e90fc63799fd824c99d8b
hotwater <vlad.dalechin@gmail.com>
2024-01-29 13:39:55 +0200

    fix: comments

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,22 @@
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  config = function()
+    -- import comment plugin safely
+    local comment = require("Comment")
+
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    -- enable comment
+    comment.setup({
+      -- for commenting tsx and jsx files
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
+  end,
+  lazy = false,
+}
